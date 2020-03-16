72dfad9a8f57a209c45da26693865afd32dbdf42
Michael Jo <michael.jo@gmail.com>
2020-03-16 14:47:40 -0700

    enable IE11

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import LoggedReducer from './LoggedReducer';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import LoggedReducer from "./LoggedReducer";
+import "react-app-polyfill/ie11";
+import 'react-app-polyfill/stable';
 
 const myStore = createStore(
   LoggedReducer,
@@ -17,7 +19,7 @@ ReactDOM.render(
   <Provider store={myStore}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
