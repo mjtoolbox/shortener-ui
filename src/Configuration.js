42b07e89c3873b13357a4a1c5be730b4132acad3
Michael Jo <michael.jo@gmail.com>
2020-03-16 07:57:11 -0700

    configuration merged

--- src/Configuration.js
+++ src/Configuration.js
@@ -1,11 +1,6 @@
 class Configuration {
-<<<<<<< HEAD
   // These URL are API URL rather than React Website URL
   API_BASE_URL = "https://shortener0312.azurewebsites.net";
   REDIRECT_URL = "https://shortener0312.azurewebsites.net/";
-=======
-  API_BASE_URL = 'https://shortener0312.azurewebsites.net';
-  REDIRECT_URL = 'https://shortener0312.azurewebsites.net/redirect/';
->>>>>>> a7d164aafafc93dc99c56a80d2e5be1ed5cfb7c2
 }
 export default Configuration;
